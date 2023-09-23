2687989cf1887b482de73b3310c3dc38358d3fa3
Steve Calla <callasteven@gmail.com>
2023-09-22 23:29:14 -0600

    Refactor WrongPage 404

--- client/src/pages/WrongPage.js
+++ client/src/pages/WrongPage.js
@@ -1,45 +1,16 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate, useHistory } from "react-router-dom";
-
-import { Home } from "./Home";
-
-import { Container, Row, Button } from "react-bootstrap/";
-import "../styles/button-style.css";
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import wrongPageImg from "../assets/images-avif/404-not-found.avif";
 
 const WrongPage = () => {
   const navigate = useNavigate();
-  // const history = useHistory();
-
-  // const homePage = () => {
-  //     history.push("/home");
-  // };
-
   return (
-    <Container>
-      <Row
-        className=" justify-content-center align-items-center "
-       
-      >
-        <Button
-          // as={Link}
-          // as={<Home />}
-          // onClick={homePage}
-          onClick={() => navigate("/home")}
-          to="/jobs-panel"
-          className="rounded-pill wrong-page-button"
-          style={{ backgroundColor: "white", border: "none" }}
-        >
-          <img
-            src={wrongPageImg}
-            className = 'not-found'
-            alt="404 Wrong Page"
-           
-          />
-        </Button>
-      </Row>
-    </Container>
+    <button
+      onClick={() => navigate('/home')}
+      style={{ backgroundColor: "white", border: "none" }}
+    >
+      <img src={wrongPageImg} className="" alt="404 Wrong Page" />
+    </button>
   );
 };
 
