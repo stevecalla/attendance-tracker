0746b3694b46dc94952db929832bea971ccfc289
Steve Calla <callasteven@gmail.com>
2023-12-04 13:40:08 -0700

    Delete old login js code

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -79,53 +79,6 @@ const Login = ({
         </div>
       </div>
     </Container>
-    
-    // <Container style={{ marginTop: "85px" }}>
-    //   <div className="d-flex flex-column align-items-center mt-3">
-    //     <div
-    //       style={{
-    //         height: "600px",
-    //         minHeight: "600px",
-    //         width: "330px",
-    //         boxShadow: "5px 5px 5px 5px gray",
-    //         overflowY: "scroll",
-    //       }}
-    //     >
-    //       <div className="mx-4 mt-4 mb-4" style={{ height: "150px" }}>
-    //         <div className="d-flex justify-content-center align-content-center align-item-center">
-    //           <div>
-    //             <img
-    //               src={littleFella}
-    //               alt="Integral Solutions logo"
-    //               style={{
-    //                 marginTop: "15px",
-    //                 maxHeight: "120px",
-    //                 maxWidth: "120px",
-    //                 backgroundColor: "black",
-    //                 borderRadius: "50%",
-    //               }}
-    //             ></img>
-    //           </div>
-    //         </div>
-    //       </div>
-    //       <div style={{ display: "flex", flex: "auto", width: "100%" }}>
-    //         <button
-    //           disabled={Auth.loggedIn()}
-    //           className={`baseline ${loginButtonIsActive && "isActive"}`}
-    //           style={{fontSize:'35px', border:'none', marginBottom:'-20px', marginTop:'-10px', color:'black'}}
-    //           onClick={() => {
-    //             navigate("/login");
-    //           }}
-    //         >
-    //           Login 
-    //         </button>
-           
-    //       </div>
-        
-    //       <LoginForm/>
-    //     </div>
-    //   </div>
-    // </Container>
   );
 };
 
