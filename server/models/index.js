9bd771fbd17f5fc0082db51b4b068a9338fc08ee
Steve Calla <callasteven@gmail.com>
2024-01-02 05:11:14 -0700

    Change order to group zoom together

--- server/models/index.js
+++ server/models/index.js
@@ -3,8 +3,8 @@ const Client = require("./Client");
 const Employee = require("./Employee");
 const Hour = require("./Hour");
 const User = require("./User");
-const ZoomUser = require("./ZoomUser");
 const EmailSend = require("./EmailSend");
+const ZoomUser = require("./ZoomUser");
 const ZoomMeeting = require("./ZoomMeeting");
 
 module.exports = { Schedule, Client, Employee, Hour, User, ZoomUser, EmailSend, ZoomMeeting };
